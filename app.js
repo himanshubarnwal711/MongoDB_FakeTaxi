293bed2907bcb7ccbe3e9bb6d02f55399b6cd923
harshi <rrai2229@gmail.com>
2022-05-29 20:20:37 +0530

    Ok

---
+++ app.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
+
+mongoose.Promise = global.Promise;
+if (process.env.NODE_ENV !== 'test') {
+	mongoose.connect('mongodb://localhost/muber');
+}
+
+app.use(bodyParser.json());
+routes(app);
+
+app.use((err, req, res, next) => {
+	res.send({ error: err.message });
+});
+
+module.exports = app;
